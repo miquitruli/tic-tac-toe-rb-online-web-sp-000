69fc3bce044ea074fc49cc2d285407ec8f605c45
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-05-06 22:36:05 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -27,33 +27,21 @@ def move(board, index, character = "X")
   return board
 end
 
-def valid_move?(board, index)
-
-  def position_taken?(board, index)
-    if (board[index] == "X" || board[index] == "O")
-      return true
-    elsif board[index] == " "
-      return false
-    elsif board[index] == ""
-      return false
-    elsif board[index] == nil
-      return false
-    end
-  end
-
-  def number_valid?(num)
-    if num.between?(0, 8) == true
-      return true
-    else
-      return false
-    end
-  end
-
-  if (position_taken?(board, index) == false) && (number_valid?(index) == true)
+def position_taken?(board, index)
+  if (board[index] == "X" || board[index] == "O")
     return true
-  else
+  elsif board[index] == " "
+    return false
+  elsif board[index] == ""
+    return false
+  elsif board[index] == nil
     return false
   end
+end
+
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
 end
 
 def turn (board)
