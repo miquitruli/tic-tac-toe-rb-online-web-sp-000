6be77688c9301a068040e140fda7f429d8db024e
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-05-06 22:27:17 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -8,3 +8,10 @@ WIN_COMBINATIONS = [
   [0,4,8], #diagonal_1
   [2,4,6] #diagonal_2
 ]
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
