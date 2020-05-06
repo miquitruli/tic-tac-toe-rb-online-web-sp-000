cc7904f0a6a2e7002e969f440ae44cd5e3510a2e
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-05-06 22:27:46 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,3 +15,9 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+def input_to_index(user_input)
+  index = user_input.to_i
+  index -= 1
+  return index
+end
