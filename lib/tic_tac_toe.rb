27a21f56fcdfd51b80a25c31b49e3954f5af2330
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-05-06 22:25:39 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,10 @@
+WIN_COMBINATIONS = [
+  [0,1,2], #Top_row
+  [3,4,5], #Middle_row
+  [6,7,8], #bottom_row
+  [0,3,6], #column_1
+  [1,4,7], #column_2
+  [2,5,8], #column_3
+  [0,4,8], #diagonal_1
+  [2,4,6] #diagonal_2
+]
